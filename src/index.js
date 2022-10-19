ef052820af64fc756990aa605d1a35c1f778a8b3
syafi <syafimohamad13@gmail.com>
2022-10-19 21:32:54 +0800

    Add function to change between tab

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ function createNavbar(){
     home.classList.add('navbarItem');
     home.classList.add('navbarSelected');
     home.textContent= 'Home';
+    home.setAttribute('data-name','home');
 
     const menu = document.createElement("div");
     menu.classList.add('navbarItem');
@@ -37,6 +38,7 @@ function createNavbar(){
     const contact = document.createElement("div");
     contact.classList.add('navbarItem');
     contact.textContent = 'Contact';
+    contact.setAttribute('data-name','contact');
 
     header.appendChild(home);
     header.appendChild(menu);
@@ -44,12 +46,38 @@ function createNavbar(){
 }
 
 function addEventListener(){
-    const menu = document.querySelector('[data-name="menu"]');
-    
-    menu.addEventListener('click', ()=> {
-        console.log('click');
-        menu.classList.add('navbarSelected');
-    });
+    const menu = document.querySelector('[data-name="menu"]'); 
+    const home = document.querySelector('[data-name="home"]');
+    const contact = document.querySelector('[data-name="contact"]'); 
+
+    menu.addEventListener('click', e=> changePage(e.target.getAttribute('data-name')));
+    home.addEventListener('click', e=> changePage(e.target.getAttribute('data-name')));
+    contact.addEventListener('click', e=> changePage(e.target.getAttribute('data-name')));
+}
+
+function changePage(page){
+
+    const menu = document.querySelector('[data-name="menu"]'); 
+    const home = document.querySelector('[data-name="home"]');
+    const contact = document.querySelector('[data-name="contact"]'); 
+
+    switch(page){
+        case 'home':
+            home.classList.add('navbarSelected');
+            menu.classList.remove('navbarSelected');
+            contact.classList.remove('navbarSelected');
+            break;
+        case 'menu':
+            home.classList.remove('navbarSelected');
+            menu.classList.add('navbarSelected');
+            contact.classList.remove('navbarSelected');
+            break;
+        case 'contact':
+            home.classList.remove('navbarSelected');
+            menu.classList.remove('navbarSelected');
+            contact.classList.add('navbarSelected');
+            break;
+    }
 }
 
 resetPage();
